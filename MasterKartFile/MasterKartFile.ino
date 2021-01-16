3d53ec7206fe72eddee76ddf33de8ed3f88f4899
Josh Moore <cosmcbun@gmail.com>
2021-01-16 15:41:54 -0600

    Changed the files back to .ino. They should work now

--- MasterKartFile/MasterKartFile.ino
+++ MasterKartFile/MasterKartFile.ino
@@ -1,9 +1,9 @@
 void setup() {
   // put your setup code here, to run once:
-
+  // This space intentionally left blank
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  // This space intentionally left blank
 }
