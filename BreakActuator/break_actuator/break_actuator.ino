a9c2be08948d26f1866eeb72e2ddafb7af69387b
Quoc Nguyen <nguyenq2@carleton.edu>
2021-01-27 19:21:42 -0600

    updated break actuator code to accomodate for mosfet

--- BreakActuator/break_actuator/break_actuator.ino
+++ BreakActuator/break_actuator/break_actuator.ino
@@ -1,14 +1,32 @@
-  int input_1 = 7;
-  int input_2 = 3;
+  int input_1 = 8;
+  int input_2 = 9;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(7, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(input_1, OUTPUT);
+  pinMode(input_2, OUTPUT);
+  retract_then_stop_actuator(5000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void retract_then_stop_actuator(int time_delay) {
+  digitalWrite(input_1, HIGH);
+  digitalWrite(input_2, LOW);
+  delay(time_delay);
+  stop_actuator();
+}
+
+void stop_actuator() {
+  digitalWrite(input_1, HIGH);
+  digitalWrite(input_2, HIGH);
+}
+
+void extend_then_stop_actuator(int time_delay) {
   digitalWrite(input_1, LOW);
   digitalWrite(input_2, HIGH);
+  delay(time_delay);
+  stop_actuator();
 }
