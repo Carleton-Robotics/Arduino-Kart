ff6d08d17fcefd305114a444d319d26181d70d30
Quoc Nguyen <nguyenq2@carleton.edu>
2021-02-06 15:04:27 -0600

    messed around with slide pot

--- BreakActuator/break_actuator/break_actuator.ino
+++ BreakActuator/break_actuator/break_actuator.ino
@@ -1,34 +1,31 @@
   int input_1 = 8;
   int input_2 = 9;
+  int slide_pot = A15;
+  int pot_val;
 
+//retracting the break actuator will increase the pot value
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(input_1, OUTPUT);
   pinMode(input_2, OUTPUT);
-  retract_then_stop_actuator(5000);
+  pinMode(slide_pot, INPUT);
+  pot_val = analogRead(slide_pot);
+  while (pot_val > 300) {
+    extend(10);
+    pot_val = analogRead(slide_pot);
+    Serial.println("Slide pot value: ");
+    Serial.println(pot_val);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-void retract_then_stop_actuator(int time_delay) {
-  digitalWrite(input_1, HIGH);
-  digitalWrite(input_2, LOW);
-  delay(time_delay);
-  stop_actuator();
-}
-
 void stop_actuator() {
   digitalWrite(input_1, HIGH);
   digitalWrite(input_2, HIGH);
-}
-
-void extend_then_stop_actuator(int time_delay) {
-  digitalWrite(input_1, LOW);
-  digitalWrite(input_2, HIGH);
-  delay(time_delay);
-  stop_actuator();
 }
 
 // input percentage as an integer value e.g. 100
@@ -37,6 +34,20 @@ void extend_by_percentage_then_stop(int percent) {
   float time_delay = percent_to_decimal * 2200;
   digitalWrite(input_1, LOW);
   digitalWrite(input_2, HIGH);
+  delay(time_delay);
+  stop_actuator();
+}
+
+void extend(int time_delay) {
+  digitalWrite(input_1, LOW);
+  digitalWrite(input_2, HIGH);
+  delay(time_delay);
+  stop_actuator();
+}
+
+void retract(int time_delay) {
+  digitalWrite(input_1, HIGH);
+  digitalWrite(input_2, LOW);
   delay(time_delay);
   stop_actuator();
 }
