d3314d3b440681cb63c78cf3cc4172e2f39ef5ce
Josh Moore <24484574+cosmcbun@users.noreply.github.com>
2021-02-06 13:25:46 -0600

    Integrated Odometer.cpp
    The code now checks with the odometer every loop, and can report mph whenever it is requested.

--- KartControl/KartControl.ino
+++ KartControl/KartControl.ino
@@ -3,14 +3,20 @@
 #import "Throttle.cpp"
 #import "Brake.cpp"
 #import "Wheel.cpp"
-
+Odometer myOdometer;
 
 void setup() {
   // put your setup code here, to run once:
   // This space intentionally left blank
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // This space intentionally left blank
+  myOdometer.pingOdometer(); //Must be run every loop
+  //if (millis()%500 == 0) { // Prints our speed every 500 ms
+  //  Serial.print("MPH: ");
+  //  Serial.println(myOdometer.readMPH());
+  //}
 }
