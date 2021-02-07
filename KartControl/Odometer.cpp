15230b5029153a2834df1f5f188ccfd4cb87654b
fredc1 <frederick.cunningham98@gmail.com>
2021-02-06 22:30:31 -0500

    change naming convention and some useability

--- KartControl/Odometer.cpp
+++ KartControl/Odometer.cpp
@@ -2,18 +2,19 @@
 
 class Odometer
 {
-  public:
-    bool toothPresent; //Quoc told me to create and give the variables values in seperate places
+  
+  private:
+    bool toothPresent; 
     float interval;
-    long updateTime;
-    int count;
+    long updateTime;//NOTE: potential for overflow after ~20 days
+    long count;//Note: potential for overflow after ~2billion teeth are read
     float pi;
     float tps;
     float rpm;
     float ipm;
-    float mph;
     int hallValue;
-    
+  public:
+    float state;
     Odometer()
     {
       // put your setup code here, to run once:
@@ -22,12 +23,12 @@ class Odometer
       updateTime = 0;
       count = 0;
       pi = 3.1415926535897932384626433832795;
-      mph = 0;
+      state = 0;
       //Serial.begin(9600); I think this is redundant now?
       pinMode(5, INPUT_PULLUP);
     }
     
-    void pingOdometer() {
+    void updateReading() {
       // put your main code here, to run repeatedly:
       hallValue = digitalRead(5);
       if (toothPresent == false && hallValue == 0) {
@@ -43,12 +44,9 @@ class Odometer
         tps = count/interval/2;
         rpm = tps/45*60; //(there are 45 teeth)
         ipm = rpm*2*3.8125*pi; //(ipm stands for inches per minute. 3.8125 in is the radius of the gear)
-        mph = ipm * 9.47/pow(10,4);
+        state = ipm * 9.47/pow(10,4);
         count = 0;
       }
     }
-    float readMPH()
-    {
-      return mph;
-    }
+    
 };
