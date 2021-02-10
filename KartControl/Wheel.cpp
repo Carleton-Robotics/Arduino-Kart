6679b667b9daad85d1af051caac27f9d98cee11d
fredc1 <frederick.cunningham98@gmail.com>
2021-02-10 18:58:08 -0500

    Update Wheel.cpp
    initial commit for wheel code

--- KartControl/Wheel.cpp
+++ KartControl/Wheel.cpp
@@ -1,2 +1,30 @@
+
 #include <Arduino.h>
-//Simon is assigned to this
+#include <Servo.h>
+//in the end we are going to want to use the serial interface with the roboclaw motor controller so this implementation is temporary.
+
+class Wheel {
+
+  private:
+    int controlPin;
+    Servo myservo;
+
+  public:
+    int state;
+    
+    Wheel(int controlPin) {
+      controlPin = controlPin;
+      myservo.attach(controlPin);
+      
+    }
+    void updateCommand(int pos){
+      myservo.write(pos);
+      state = pos;
+    }
+
+    void updateReading(){
+      //empty for now until we have the other library up and running
+    }
+    
+  
+}
