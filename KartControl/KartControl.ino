cf18f73e15a2659b23275ade456417379c59af0f
fredc1 <frederick.cunningham98@gmail.com>
2021-02-10 21:47:37 -0500

    Update KartControl.ino
    hjj

--- KartControl/KartControl.ino
+++ KartControl/KartControl.ino
@@ -7,6 +7,7 @@
 //this code is basically a switching yard for information between the kart and the phone
 Odometer odometer(pin=5);
 Throttle throttle(switchPin = );//note the I2C pins are hardcoded into the library we are extending for the digipot
+Wheel wheel(controlPin = )
 //Brake brake(controlPin1 = ,controlPin2 = ,potentiometerPin = ,endOfTravelValue = );
 
 void setup() {
@@ -19,20 +20,24 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   // This space intentionally left blank
-  updateSensorVals();
+  updateStateVariables();
   updateCommands();
 }
 
-void updateSensorVals(){
-  //update sensor vals
+//this is where you make sure the object state variables are set to the correct values
+void updateStateVariables(){
+  
   odometer.updateReading();
   throttle.updateReading();
-  //brake.updateReading();
+  wheel.updateReading();
+  brake.updateReading();
 }
+
+//here is where you update your demands for all of the kart peripherals. Each function takes a 0-1000 which is mapped to required range in classes integer input
 void updateCommands(){
-  //update command states
-  throttle.updateCommand(0);//input 0 or 1 based on if the throttle should be engaged or not (this is an artifact of the analog controller)
-  //brake.updateCommand(input value from bluetooth);
+  throttle.updateCommand(100);
+  wheel.updateCommand(100);
+  brake.updateCommand(500);
 }
   
   
