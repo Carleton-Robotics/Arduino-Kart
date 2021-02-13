582821c9f33b5f4bdd3b1466ec9da343a34965f0
fredc1 <frederick.cunningham98@gmail.com>
2021-02-13 15:00:44 -0600

    Update Brake.cpp
    this works. don't change

--- KartControl/Brake.cpp
+++ KartControl/Brake.cpp
@@ -1,68 +1,50 @@
 #include <Arduino.h>
-//quoc is assigned to this but Josh is doing it instead :p
+
 
 class Brake {
   private:
-    int input_1; //for the brake mechanism
-    int input_2;
-    
-    unsigned char pot; //for the potentiometer
-    int sensorValue;
-    int brakeMin;
-    int brakeMax;
-    int currentPrecentage;
-    int targetPrecentage;
-
+    int control1;
+    int control2;
+    int pot;
+    int endOfTravelValue;
+    int startOfTravelValue;
   public:
-    Brake(int controlPin1,int controlPin2, unsigned char potentiometerPin) {
-      input_1 = controlPin1;
-      input_2 = controlPin2;
+    int state;
+    Brake(int controlPin1,int controlPin2, int potentiometerPin){
+      control1 = controlPin1;
+      control2 = controlPin2;
       pot = potentiometerPin;
-      pinMode(input_1, OUTPUT);
-      pinMode(input_2, OUTPUT);
-      pinMode(potentiometerPin, INPUT); //Might be unnessessary?
-      sensorValue = analogRead(pot);
-      removeBrake();
-      brakeMin = 0; //These are tentative and depend on the physical device's installation
-      brakeMax = 1024;
-      targetPrecentage = 0;
+      pinMode(control1,OUTPUT);
+      pinMode(control2, OUTPUT);
+      state = map(analogRead(pot),0,1024,0,100);
+      endOfTravelValue = 100;
+      startOfTravelValue = 900;
+      updateCommand(0);
+      
       //initialize pins for control and potentiometer, set to zero-brakes condition, 
     }
-    void updateReading() {
-      sensorValue = analogRead(pot);
-      currentPrecentage = (sensorValue-brakeMin)/(brakeMax-brakeMin);
-      //if target<state set the relay into the position that will make it move in the desired direction
-      //else if target>state set the relay into a position that will make it move int the desired direction
-      if (currentPrecentage < targetPrecentage) {
-        digitalWrite(input_1, LOW);
-        digitalWrite(input_2, HIGH);
-      }
-      else if (currentPrecentage > targetPrecentage) {
-        digitalWrite(input_1, HIGH);
-        digitalWrite(input_2, LOW);
-      }
-      else if (currentPrecentage = targetPrecentage) {
-        digitalWrite(input_1, HIGH);
-        digitalWrite(input_2, HIGH);
-      }
+
+    
+    void updateReading(){
+      state = map(analogRead(pot), startOfTravelValue, endOfTravelValue,0,1000);
     }
+    
     void updateCommand(int target){
-      targetPrecentage = target;
-    }
-    void removeBrake() {
-      //stop entire program (with while loop) to move the brake back until state is zero
-      while (currentPrecentage > 0) {
-        currentPrecentage = (sensorValue-brakeMin)/(brakeMax-brakeMin);
-        digitalWrite(input_1, HIGH);
-        digitalWrite(input_2, LOW);
+      //scaledTarget = map(target,0,100,endOfTravelValue,startOfTravelValue);
+      if (sqrt(sq(target-state))<10){
+        digitalWrite(control1,HIGH);
+        digitalWrite(control2,HIGH);
+        
       }
-    }
-    void emergencyBrake() {
-      //stop entire program (with while loop) to move the brake into fully braked position until reaches val
-      while (currentPrecentage < 100) {
-        currentPrecentage = (sensorValue-brakeMin)/(brakeMax-brakeMin);
-        digitalWrite(input_1, LOW);
-        digitalWrite(input_2, HIGH);
+      else if (target>state){
+        digitalWrite(control1,LOW);
+        digitalWrite(control2,HIGH);
       }
+      else if(target<state){
+        digitalWrite(control1, HIGH);
+        digitalWrite(control2, LOW);
+      }
+      
+      
     }
 };
