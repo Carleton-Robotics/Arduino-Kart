c848de799403f31e573421120242bae27631569f
evanarch <evantarch@gmail.com>
2022-06-03 17:20:08 -0500

    Cleanup

--- KartControl/KartControl.ino
+++ KartControl/KartControl.ino
@@ -93,25 +93,25 @@ void printGPSCoords(){
   }
 }
 
-void display_freeram() {
+void display_freeram(){
   Serial.print(F("- SRAM left: "));
   Serial.println(freeRam());
 }
 
-int freeRam() {
+int freeRam(){
   extern int __heap_start,*__brkval;
   int v;
   return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int) __brkval);  
 }
 
 void eStop(){
-  //steeringMotor.eStop();
+  steeringMotor.eStop();
   speedController.eStop();
   while(1);
 }
 
 void stop(){
-  //steeringMotor.eStop();
+  steeringMotor.eStop();
   speedController.eStop();
   resetFunc();
 }
