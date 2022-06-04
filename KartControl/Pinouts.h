4b3871273bc890af4456361f8ba106db1a7de12c
evanarch <evantarch@gmail.com>
2022-06-03 19:18:27 -0500

    Updated Pinouts

--- KartControl/Pinouts.h
+++ KartControl/Pinouts.h
@@ -1,34 +1,44 @@
-// Bluetooth
+//** Bluetooth **
 #define BluetoothModePin 23
 #define BluetoothGroundPin 3 //TODO Swith to Pin 4
 // Serial1
     // Tx1 = 18
     // Rx1 = 19
 
-// SteeringMotor
+//** Brake **
+#define BrakePin1 8
+#define BrakePin2 9
+#define BrakePotentiometerPin A1
+
+//** Compass **
+// I2C
+    // SDA = 20
+    // SCL = 21
+
+//** Control Box Switch **
+#define MiscPin A0 
+
+//** GPS **
+// I2C
+    // SDA = 20
+    // SCL = 21
+
+//** Odometer **
+#define OdometerPin 2
+
+//** SteeringMotor **
 // Serial2
     // Tx2 = 16
     // Rx2 = 17
 
-// Throttle
+//** Throttle **
 #define ThrottleSwitchPin 3
 // I2C
     // SDA = 20
     // SCL = 21
     
-// SD Card Reader
+//** SD Card Reader **
 // CS - 53
 // DI - 51
 // DO - 50
 // CLK - 52
-
-
-// Odometer
-#define OdometerPin 2
-
-// Brake
-#define BrakePin1 8
-#define BrakePin2 9
-#define BrakePotentiometerPin A1
-
-#define MiscPin A0 //Control Box Switch
